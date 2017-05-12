21497e99e2df4c0b3b69253fc1cfb9921ffed244
Kristian Mandrup <kmandrup@gmail.com>
2017-05-12 16:06:30 +0200

    add test skeletons

--- test/compare-nodes.test.js
+++ test/compare-nodes.test.js
@@ -8,11 +8,26 @@ function pretty(obj) {
   return JSON.stringify(obj, null, 2)
 }
 
-test('compare', t => {
+test('createCompareNode', t => {
+  let result = createCompareNode(list)
+  t.fail()
+})
+
+test('createNodeMatcher', t => {
+  let result = createNodeMatcher(node)
+  t.fail()
+})
+
+test('prepareNode', t => {
+  let result = prepareNode(node)
+  t.fail()
+})
+
+test('compareAll', t => {
   let result = compareAll(nodes.first, nodes.mix)
   console.log('RESULT')
   console.log('ops', pretty(result.ops), {
     deleted: result.deleted
   })
-  t.pass()
+  t.fail()
 })
